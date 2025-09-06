79359e8887d31b6d5e901dd0797deb566fa82180
Firegame224 <xg474627@gmail.com>
2025-09-06 17:48:51 +0700

    setup

---
+++ app/routes/hello.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hello() {
+  return (
+    <div className="bg-red-500 text-white font-semibold">
+      <p>Hello world</p>
+    </div>
+  );
+}
---
+++ app/routes/page.layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function IniPage() {
+  return (
+    <div className='bg-white'>
+      
+    </div>
+  )
+}
