0b56a079ac32aea19b62ef674c3f783da75740bb
Firegame224 <xg474627@gmail.com>
2025-09-15 23:46:24 +0700

    feat(profile-hook, profile-service): add updateProfile(name,bio) hooks & service to

--- app/features/profile/hooks/profile-hook.ts
+++ app/features/profile/hooks/profile-hook.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query"
 import { profileService } from "../service/profile-service"
 
 export const useGetProfile = () => {
@@ -6,5 +6,33 @@ export const useGetProfile = () => {
         queryKey: ["profile"],
         queryFn: async () => await profileService.getProfile(),
         staleTime: 1000 * 60 * 1
+    })
+}
+
+export const usePatchName = () => {
+    const queryClient = new QueryClient()
+    return useMutation({
+        mutationKey: ["patch-name"],
+        mutationFn: async (data: { userName: string }) => await profileService.updateName(data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["profile"], exact: true })
+        },
+        onError: (err) => {
+            alert(err.message || "Gagal ubah nama")
+        }
+    })
+}
+
+export const usePatchBio = () => {
+    const queryClient = new QueryClient()
+    return useMutation({
+        mutationKey: ["patch-name"],
+        mutationFn: async (data: { bio: string }) => await profileService.updateBio(data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["profile"], exact: true })
+        },
+        onError: (err) => {
+            alert(err.message || "Gagal ubah bio")
+        }
     })
 }
--- app/features/profile/service/profile-service.ts
+++ app/features/profile/service/profile-service.ts
@@ -4,5 +4,11 @@ import type { UserType } from "shared/types/user-type"
 export const profileService = {
     async getProfile() {
         return await apiClient<{ data: UserType }>({ url: "/profile/get", withCredentials: true })
+    },
+    async updateName(data: { userName: string }) {
+        return await apiClient<{ message: string, statusCode: number }>({ url: "/profile/name/patch", method: "patch", data, withCredentials: true })
+    },
+    async updateBio(data: { bio: string }) {
+        return await apiClient<{ message: string, statusCode: number }>({ url: "/profile/bio/patch", method: "patch", data, withCredentials: true })
     }
 } 
