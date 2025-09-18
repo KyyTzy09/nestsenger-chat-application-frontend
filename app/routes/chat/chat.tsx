7c9be7b92b78e68f60c28ad5d6cb4f90c8619142
Firegame224 <xg474627@gmail.com>
2025-09-18 23:18:32 +0700

    feat(component): add reusable image-uploader component for upload image feature

--- app/routes/chat/chat.tsx
+++ app/routes/chat/chat.tsx
@@ -4,13 +4,17 @@ import type { Route } from "../+types";
 import { profileService } from "~/features/profile/service/profile-service";
 import { redirect, useLoaderData } from "react-router";
 import { getSession } from "~/features/auth/services/get-session";
+import ImageCropper from "shared/components/image-uploader";
 
 export default function ChatIndex() {
+  const [image, setImage] = React.useState<Blob | null>(null)
+  console.log(image)
   return (
     <div className="flex flex-col items-center justify-center w-full h-full min-h-screen bg-[#232323] text-gray-400">
       <FaWhatsapp className="w-10 h-10" />
       <p className="text-white text-lg font-semibold">Nestsenger</p>
       <p className="text-sm">Belum ada chat yang dibuka</p>
+      <ImageCropper setimageUpload={setImage} aspect={1/1} cropShape="rect"/>
     </div>
   );
 }
