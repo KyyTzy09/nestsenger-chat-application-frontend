67e8a8e6d20af585d93d8451bc94362f6d31989b
Firegame224 <xg474627@gmail.com>
2025-10-15 22:52:08 +0700

    feat(room): add animation in room-card for better ux

--- app/features/room/components/cards/room-card.tsx
+++ app/features/room/components/cards/room-card.tsx
@@ -6,6 +6,7 @@ import type { FriendType } from "shared/types/friend-type";
 import type { RoomType } from "shared/types/room-type";
 import type { UserType } from "shared/types/user-type";
 import RoomLastChatSection from "../section/room-lastchat-section";
+import { motion } from "motion/react";
 
 interface RoomCardProps {
   data:
@@ -22,25 +23,35 @@ export default function RoomCard({ data }: RoomCardProps) {
     <div className="flex flex-col items-center justify-start w-full h-full gap-2">
       {data?.map(({ room, alias }, index) => {
         return (
-          <Link
-            to={`/chat/${room.roomId}`}
-            key={index}
-            className={`${params.chatId === room.roomId ? "bg-[#45494f]" : "bg-transparent"} flex items-center justify-start w-full h-[70px] rounded-sm p-2 gap-2 hover:bg-[#45494f]/50`}
+          <motion.div
+            className={`${params.chatId === room.roomId ? "bg-[#45494f]" : "bg-transparent"} flex items-center justify-start w-full h-[70px] rounded-sm p-2 hover:bg-[#45494f]/50`}
+            
+            initial={{ translateY: 20, opacity: 0 }}
+            animate={{ translateY: 0, opacity: 1 }}
+            exit={{ opacity: 0, translateY: -20 }}
+            transition={{ duration: 0.5 }}
           >
-            <section className="w-[65px] h-full rounded-full overflow-hidden">
-              <img
-                src={
-                  alias && room.type === RoomTypeEnum.PRIVATE
-                    ? (alias as FriendType)?.friend?.avatar ||
-                      (alias as UserType)?.profile?.avatar
-                    : room.avatar || defaultImage
-                }
-                alt="Default"
-                className="w-full h-full"
-              />
-            </section>
-            <RoomLastChatSection room={room!} alias={alias!} />
-          </Link>
+            {" "}
+            <Link
+              to={`/chat/${room.roomId}`}
+              key={index}
+              className="flex w-full h-full gap-2"
+            >
+              <section className="w-[65px] h-full rounded-full overflow-hidden">
+                <img
+                  src={
+                    alias && room.type === RoomTypeEnum.PRIVATE
+                      ? (alias as FriendType)?.friend?.avatar ||
+                        (alias as UserType)?.profile?.avatar
+                      : room.avatar || defaultImage
+                  }
+                  alt="Default"
+                  className="w-full h-full"
+                />
+              </section>
+              <RoomLastChatSection room={room!} alias={alias!} />
+            </Link>
+          </motion.div>
         );
       })}
     </div>
