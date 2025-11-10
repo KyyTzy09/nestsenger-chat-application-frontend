849e359f15d89a47ceace1423ecf40691f319385
Firegame224 <xg474627@gmail.com>
2025-11-11 00:55:12 +0700

    refactor(type): update chatMediaType for api response type safety

--- shared/types/media-type.ts
+++ shared/types/media-type.ts
@@ -2,7 +2,9 @@ import type { ChatType } from "./chat-type"
 
 export type ChatMediaType = {
     chatId: string
+    mediaName: string
     mediaUrl: string
+    size: string
     createdAt: Date
     updatedAt: Date
 
