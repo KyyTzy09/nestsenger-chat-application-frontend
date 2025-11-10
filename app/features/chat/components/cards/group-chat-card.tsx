cc96ffc51509fda99b3ec28f27a98cbac07b0df4
Firegame224 <xg474627@gmail.com>
2025-11-10 23:53:24 +0700

    refactor(chat): update card size for better ui

--- app/features/chat/components/cards/group-chat-card.tsx
+++ app/features/chat/components/cards/group-chat-card.tsx
@@ -114,7 +114,7 @@ export default function GroupChatCard({
               )}
               <section
                 onContextMenu={(e) => handleShowContextMenu(e, chatId)}
-                className={`${senderId === currentUserId ? "bg-blue-500 rounded-tr-none" : "bg-[#303030] rounded-tl-none"} relative flex flex-col  ${media ? "w-[40%]" : "w-auto"} max-w-[55%] min-w-[120px] h-auto text-white p-2 rounded-sm shadow`}
+                className={`${senderId === currentUserId ? "bg-blue-500 rounded-tr-none" : "bg-[#303030] rounded-tl-none"} relative flex flex-col  ${media ? "w-[35%]" : "w-auto"} max-w-[55%] min-w-[120px] h-auto text-white p-2 rounded-sm shadow`}
               >
                 {senderId !== currentUserId && (
                   <div className="flex items-center w-full">
