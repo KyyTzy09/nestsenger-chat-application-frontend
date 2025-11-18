ecfe9393797d79f414873dc0f8ce309f577831b8
Firegame224 <xg474627@gmail.com>
2025-11-19 00:29:56 +0700

    style(chat-parent): enhance layout and media display in chat parent section

--- app/features/chat/components/sections/chat-parent-section.tsx
+++ app/features/chat/components/sections/chat-parent-section.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useGetChatParent } from "../../hooks/chat-hook";
 import type { FriendType } from "shared/types/friend-type";
 import type { UserType } from "shared/types/user-type";
+import { FileIcon, MusicIcon } from "lucide-react";
 
 interface ChatParentSectionProps {
   currentUserId: string;
@@ -16,11 +17,32 @@ export default function ChatParentSection({
   const { chat, user } = chatParentResponse?.data || {};
 
   return (
-    <section className="flex flex-col items-start justify-between w-full h-auto p-2 bg-gray-500/40 rounded-md border-l-[5px] border-blue-700">
-      <p className="flex items-center justify-start w-full text-[12px] text-gray-300 font-semibold">
-        {user && currentUserId === chat?.userId ? "Anda" : user?.alias}
-      </p>
-      <p className="line-clamp-1 text-[11px] text-gray-300">{chat?.message}</p>
+    <section className="flex items-center justify-between w-full h-auto bg-gray-500/40 rounded-md border-l-[5px] border-blue-700">
+      <div className="flex flex-col items-start justify-between w-auto h-full p-2">
+        <p className="flex items-center justify-start w-full text-[12px] text-gray-300 font-semibold">
+          {user && currentUserId === chat?.userId ? "Anda" : user?.alias}
+        </p>
+
+        <p className="flex items-center justify-start line-clamp-1 text-[11px] text-gray-300 gap-1">
+          {chat?.media && chat?.media?.mediaType === "audio" ? (
+            <MusicIcon className="w-4 h-4" />
+          ) : chat?.media?.mediaType === "file" ? (
+            <FileIcon className="w-4 h-4" />
+          ) : null}
+          {chat?.message}
+        </p>
+      </div>
+      <div className="w-12 h-full rounded-r-sm overflow-hidden">
+        {chat?.media && chat?.media?.mediaType === "image" ? (
+          <img
+            title="media"
+            className="w-full h-full object-cover"
+            src={chat?.media.mediaUrl}
+          />
+        ) : chat?.media?.mediaType === "video" ? (
+          <video className="w-full h-full" src={chat?.media.mediaUrl}></video>
+        ) : null}
+      </div>
     </section>
   );
 }
