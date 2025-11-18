60a83f02fd6fef078741fb18e3371d39cb9180fc
Firegame224 <xg474627@gmail.com>
2025-11-18 23:53:56 +0700

    feat(chat-media): implement useGetNonFile hook to get files data

--- app/features/chat/components/chat-media-preview.tsx
+++ app/features/chat/components/chat-media-preview.tsx
@@ -1,17 +1,23 @@
-import { ArrowLeftIcon } from "lucide-react";
+import { ArrowLeftIcon, Music2Icon } from "lucide-react";
 import { motion } from "motion/react";
 import React from "react";
 import { Button } from "shared/shadcn/button";
+import { useGetNonFileMedia } from "../hooks/chat-media-hook";
+import { useParams } from "react-router";
 
 export default function ChatMediaPreview() {
   const isOpen = false;
+  const { roomId } = useParams<{ roomId: string }>() as { roomId: string };
+  const { data: nonFileMediaResponse, isPending } = useGetNonFileMedia({
+    roomId,
+  });
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0 }}
       className={`${
-        isOpen ? "flex" : "hidden"
+        isOpen && roomId ? "flex" : "hidden"
       } fixed flex-col w-full min-h-screen bg-[#232323]/80 z-50 top-0 bottom-0 left-0 right-0 backdrop-blur-md gap-1 px-3 py-2`}
     >
       <section className="flex items-center justify-between w-full">
@@ -23,13 +29,31 @@ export default function ChatMediaPreview() {
         <div className="w-[30%] h-full bg-gray-500"></div>
       </section>
       <section className="flex flex-row items-center w-full max-h-[10%] py-1 gap-2 overflow-x-auto no-scrollbar">
-        {Array.from({ length: 101 }).map((v, i) => {
-          return (
-            <div key={i} className="min-w-12 h-12 bg-white rounded-sm border-blue-500 border-3">
-              {i}
-            </div>
-          );
-        })}
+        {nonFileMediaResponse?.data?.length! > 0 &&
+          nonFileMediaResponse?.data?.map(
+            ({ mediaName, mediaType, mediaUrl }, i) => {
+              return (
+                <div
+                  key={i}
+                  className="min-w-12 h-12 rounded-sm border-blue-500 border-3"
+                >
+                  {mediaType === "image" ? (
+                    <img
+                      className="w-full h-full object-cover"
+                      src={mediaUrl}
+                      alt={mediaName}
+                    />
+                  ) : mediaType === "video" ? (
+                    <video src={mediaUrl} className="w-full h-full object-contain"></video>
+                  ) : (
+                    <div className="w-full h-full p-1">
+                      <Music2Icon className="w-full h-full " />
+                    </div>
+                  )}
+                </div>
+              );
+            }
+          )}
       </section>
     </motion.div>
   );
