e262447c43c8a803b02d62b65f9bb8de55256014
Firegame224 <xg474627@gmail.com>
2025-12-09 20:36:56 +0700

    feat(status): implement useGetTodayStatuses hook to get status data

---
+++ app/features/status/services/status-service.ts
@@ -0,0 +1,9 @@
+import { apiClient } from "shared/helpers/axios"
+import type { AliasType } from "shared/types/alias-type"
+import type { StatusType } from "shared/types/status-type"
+
+export const StatusService = {
+    async getTodayStatuses() {
+        return await apiClient<{ data: { alias: AliasType, statuses: StatusType[] }[] }>({ url: "/status/today/get", withCredentials: true })
+    }
+}
