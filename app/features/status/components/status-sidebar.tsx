9a090825847b7bcbaed5ec58fc6d349d83fedd5c
Firegame224 <xg474627@gmail.com>
2025-12-09 20:45:58 +0700

    feat(status): integrate statusSidebarCard with data from api

--- app/features/status/components/status-sidebar.tsx
+++ app/features/status/components/status-sidebar.tsx
@@ -10,8 +10,10 @@ import { socket } from "shared/configs/socket";
 import { useQueryClient } from "@tanstack/react-query";
 import { FaWhatsapp } from "react-icons/fa";
 import { defaultImage } from "shared/constants/image-default";
+import { useGetTodayStatuses } from "../hooks/status-hook";
 
 export default function StatusSidebar() {
+  const { data: statusesTodayResponse } = useGetTodayStatuses();
   return (
     <aside className="relative z-0 flex flex-col w-full h-full bg-[#252525] pt-10 text-white gap-4">
       <div className="flex items-center justify-between w-full px-5">
@@ -52,29 +54,31 @@ export default function StatusSidebar() {
             Pembaruan Terkini
           </Label>
           <div className="flex flex-col w-full h-full gap-2">
-            {Array.from({ length: 100 }).map(() => {
+            {statusesTodayResponse?.data?.map(({ alias, statuses }) => {
               return (
                 <div className="flex items-center justify-start w-full h-16 gap-3 py-2">
                   <div className="w-14 h-14 border-[3px] border-blue-500 rounded-full">
                     <img
-                      src={
-                        "https://i.pinimg.com/736x/8d/b9/77/8db9773d4e8576969efbffa06e35a878.jpg"
-                      }
-                      alt={"profile"}
+                      src={alias ? alias.avatar : defaultImage}
+                      alt={defaultImage}
                       className="w-full h-full object-cover rounded-full"
                     />
                   </div>
                   <div className="flex flex-col items-start justify-center h-full text-white">
-                    <p className="font-semibold text-[14px]">Fiky Ganteng</p>
+                    <p className="font-semibold text-[14px]">{alias.alias}</p>
                     <p className="text-sm">
-                      {new Date().toLocaleDateString("id-ID", {
+                      {new Date(
+                        statuses[statuses.length - 1]?.createdAt
+                      ).toLocaleDateString("id-ID", {
                         weekday: "long",
                       })}
                       {", "}
-                      {new Date().toLocaleTimeString("id-ID", {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      })}
+                      {new Date(statuses[statuses.length - 1]?.createdAt)
+                        .toLocaleTimeString("id-ID", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })
+                        .replace(".", ":")}
                     </p>
                   </div>
                 </div>
