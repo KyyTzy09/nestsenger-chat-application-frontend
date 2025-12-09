f8fd24cdcece13bad9c131bf12ad879b4383c66e
Firegame224 <xg474627@gmail.com>
2025-12-09 20:28:35 +0700

    feat(status): define StatusType and statusViewerType for better type safety

---
+++ shared/types/status-type.ts
@@ -0,0 +1,32 @@
+import type { FriendType } from "./friend-type"
+import type { UserType } from "./user-type"
+
+export type StatusType = {
+    statusId: string
+    mediaName: string | null
+    mediaUrl: string
+    mediaType: "image" | "video" | "audio" | "file"
+    message: string | null
+
+    createdAt: Date
+    creatorId: string
+
+    creator: UserType
+    viewers: StatusViewer[]
+}
+
+export type StatusViewer = {
+    viewerId: string
+
+    statusId: string
+    friendId: string
+
+    isViewed: boolean | false
+
+    createdAt: Date
+    updatedAt: Date
+
+    status: StatusType
+    friend: FriendType
+}
+
