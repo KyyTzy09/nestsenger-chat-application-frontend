30851571ec00a0d720672e1c3c0f1b0f1b5d3892
Firegame224 <xg474627@gmail.com>
2025-12-14 02:04:36 +0700

    feat(status): implement hook to update statusview

--- app/features/status/hooks/status-viewer-hook.ts
+++ app/features/status/hooks/status-viewer-hook.ts
@@ -1,10 +1,26 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { ViewerService } from "../services/status-viewer-service"
+import { toast } from "sonner"
 
 export const useGetTodayUserViewers = () => {
     return useQuery({
         queryKey: ['viewers-user'],
         queryFn: async () => await ViewerService.getTodayUserViewers(),
         staleTime: 2000 * 60 * 60
+    })
+}
+
+export const useUpdateViewStatus = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationKey: ['update-status'],
+        mutationFn: async (data: { viewerId: string, statusId: string }) => await ViewerService.updateViewStatus(data),
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ['status-viewer', data?.data?.statusId], type: "all" })
+            queryClient.invalidateQueries({ queryKey: ['viewers-user'], type: "all" })
+        },
+        onError: (err) => {
+            toast.error(err.message)
+        }
     })
 }
--- app/features/status/services/status-viewer-service.ts
+++ app/features/status/services/status-viewer-service.ts
@@ -4,5 +4,8 @@ import type { StatusViewer } from "shared/types/status-type"
 export const ViewerService = {
     async getTodayUserViewers() {
         return await apiClient<{ data: StatusViewer[] }>({ url: "/viewer/today-user/get", withCredentials: true })
+    },
+    async updateViewStatus(data: { viewerId: string, statusId: string }) {
+        return await apiClient<{ message: string, data: StatusViewer | null }>({ url: "/viewer/update/patch", data, withCredentials: true })
     }
 }
